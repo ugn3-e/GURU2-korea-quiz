0a001324ee1c4489682a0d9a6db9198e5241d856
ugn3-e <sej20080257@gmail.com@example.com>
2026-01-19 04:24:25 +0900

    최종 결과 화면 구현

---
+++ app/src/main/java/com/example/guru2/SlangFinalResultActivity.kt
@@ -0,0 +1,37 @@
+package com.example.guru2
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class SlangFinalResultActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_slang_final_result)
+
+        // 디미 유진_결과 요약 텍스트
+        val tvSummary = findViewById<TextView>(R.id.tvSummary)
+
+        // 디미 유진_홈으로 돌아가기 버튼
+        val btnFinish = findViewById<Button>(R.id.btnFinish)
+
+        val total = intent.getIntExtra("totalCount", 0)
+        val correct = intent.getIntExtra("correctCount", 0)
+
+        // 디미 유진_퀴즈 결과 요약 메세지
+        tvSummary.text = """
+            🎉 오늘의 모든 퀴즈를 완료했습니다!
+            수고하였습니다!
+            
+            총 문제 수 : $total
+            맞힌 문제 수 : $correct
+        """.trimIndent()
+
+        // 디미 유진_SlangQuizActivity로 이동
+        btnFinish.setOnClickListener {
+            finish()
+        }
+    }
+}
