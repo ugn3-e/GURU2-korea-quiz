ac9036321e9b53122b0383fcb1adb09dbe901779
ugn3-e <sej20080257@gmail.com@example.com>
2026-01-22 14:37:32 +0900

    국적 선택 수정

--- app/src/main/java/com/example/guru2/login/SignupActivity.kt
+++ app/src/main/java/com/example/guru2/login/SignupActivity.kt
@@ -8,6 +8,7 @@ import com.example.guru2.auth.AuthRepository
 import com.example.guru2.auth.SQLiteAuthDataSource
 import android.content.Intent
 import com.example.guru2.login.LoginActivity
+import java.util.Locale
 
 class SignupActivity : AppCompatActivity() {
 
@@ -23,7 +24,28 @@ class SignupActivity : AppCompatActivity() {
         val etAge = findViewById<EditText>(R.id.etAge)
 
         val spGender = findViewById<Spinner>(R.id.spGender)
+
         val spCountry = findViewById<Spinner>(R.id.spCountry)
+
+        /* 국적 Spinner 동적 세팅 */
+        val countryList = Locale.getISOCountries()
+            .map { code ->
+                Locale("", code).displayCountry
+            }
+            .filter { it.isNotBlank() }
+            .distinct()
+            .sorted()
+
+        val countryAdapter = ArrayAdapter<String>(
+            this,
+            android.R.layout.simple_spinner_item,
+            countryList
+        )
+
+        countryAdapter.setDropDownViewResource(
+            android.R.layout.simple_spinner_dropdown_item
+        )
+        spCountry.adapter = countryAdapter
 
         findViewById<Button>(R.id.btnSignup).setOnClickListener {
 
@@ -39,7 +61,7 @@ class SignupActivity : AppCompatActivity() {
             if (success) {
                 Toast.makeText(this, "회원가입 완료", Toast.LENGTH_SHORT).show()
 
-                // ✅ 로그인 화면으로 이동
+                // 로그인 화면으로 이동
                 val intent = Intent(this, LoginActivity::class.java)
                 startActivity(intent)
 
